dc16cbf998f751a2bd408069e4512402ea50c4db
Cavin Gayle <cavingayle@Cavins-MacBook-Pro.local>
2021-05-31 10:47:25 -0400

    initial

---
+++ options.js
@@ -0,0 +1,22 @@
+const nameInput = document.getElementById("name-input");
+const saveBtn = document.getElementById("save-btn");
+const timeInput = document.getElementById("time-input");
+
+saveBtn.addEventListener("click", () => {
+  const name = nameInput.value;
+  const notificationTime = timeInput.value;
+  chrome.storage.sync.set(
+    {
+      name,
+      notificationTime,
+    },
+    () => {
+      console.log(`Name set to ${name}`);
+    }
+  );
+});
+
+chrome.storage.sync.get(["name", "notificationTime"], (res) => {
+  nameInput.value = res.name ?? "???";
+  timeInput.value = res.notificationTime ?? 1000;
+});
